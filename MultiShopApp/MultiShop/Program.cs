7d41b759a2ea81cd7b4ea1cc47a5263b71a26917
KamranTalis <az.kamik.rus@gmail.com>
2024-05-07 13:12:24 +0400

    add

---
+++ MultiShopApp/MultiShop/Program.cs
@@ -0,0 +1,21 @@
+namespace MultiShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.MapControllerRoute
+                (
+                "default",
+                "{controller=Home}/{action=Index}"
+                );
+
+            app.Run();
+        }
+    }
+}
